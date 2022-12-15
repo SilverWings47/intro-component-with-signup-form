025e5f688f57ad3fae374659099e74322e5a41fa
SilverWings47 <nasrisami17@gmail.com>
2022-12-15 19:08:54 +0100

    Improved form validation process

--- js/main.js
+++ js/main.js
@@ -1,29 +1,56 @@
+const alertBox = document.querySelector('.alert-box');
+const closeBtn = document.querySelector('.close-btn');
 const form = document.querySelector('form');
-const inputs = document.querySelectorAll('input');
+let inputs = document.querySelectorAll('input');
 
-let isValid;
+const emailRegex = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+const textRegex = /^[A-Za-z]{4,}(?!\d)$/;
+
+const validateInput = (input) => {
+  if (input.value.length === 0) {
+    setInvalid(input, `${input.placeholder} cannot be empty`);
+    return false;
+  }else if(input.type === 'email' && !emailRegex.test(input.value)) {
+    setInvalid(input, 'Doesn\'t look like an email');
+    return false;
+  }else if(input.type === 'text' && !textRegex.test(input.value)) {
+    setInvalid(input, 'Only letters allowed');
+    return false;
+  }else if(input.type === 'password' && input.value.length < 8) {
+    setInvalid(input, `${input.placeholder} should be at least 8 characters long`);
+    return false;
+  }else {
+    input.classList.remove('submitted');
+    return true;
+  }
+};
+
+const setInvalid = (input, errorMsg) => {
+  input.nextElementSibling.textContent = errorMsg;
+  input.classList.add('submitted');
+};
+
+const validateForm = (event) => {
+  event.preventDefault();
+
+  inputs = Array.from(inputs);
+  
+  const isValidated = inputs.map((input) => {
+    return validateInput(input);
+  });
+
+  if(isValidated.includes(false)) return;
+
+  form.submit();
+  alertBox.classList.add('active');
+
+  inputs.forEach((input) => {
+    input.value = '';
+  });
+};
 
 form.addEventListener('submit', validateForm);
 
-function validateForm(e) {
-  e.preventDefault();
-  isValid = true;
-
-  inputs.forEach(input => {
-      if(input.validity.valueMissing || input.validity.typeMismatch){
-        isValid = false;
-        input.classList.add('submitted');
-      }
-  });
-  
-  if(isValid) {
-    console.log('Submitted');
-    form.submit();
-    isValid = true;
-
-    inputs.forEach((input) => {
-      input.classList.remove('submitted');
-      input.value = '';
-    });
-  } 
-}
+closeBtn.addEventListener('click', () => {
+  alertBox.classList.remove('active');
+});
